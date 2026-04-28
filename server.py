2adf7b109bf6a608faa27ffb50fdbf091ecc42b6
leesodi <woodisodi@gmail.com>
2026-04-28 18:45:51 +0900

    update

--- server.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-os.chdir('/Users/user/Desktop/maker')
-sys.argv = ['server.py', '3000']
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-httpd = HTTPServer(('', 3000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
